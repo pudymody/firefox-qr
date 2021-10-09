ac60fcf0c75fae9065edeae232f903c2c599cfe7
Federico Scodelaro <federicoscodelaro@gmail.com>
2021-10-09 00:23:22 -0300

    feat: Allow png download of qr

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const $text = document.querySelector("input");
 const $qr = document.querySelector(".qr");
-const $save = document.querySelector(".save");
+const $exports = document.querySelectorAll(".actions a");
+const SIZE = 800;
 
 function drawQr(text){
 	let fg = "#2a2a2e";
@@ -13,11 +14,29 @@ function drawQr(text){
 		background: bg,
 		join: true,
 		xmlDeclaration: false,
-		container: "svg-viewbox"
+		container: "g",
+		width: SIZE,
+		height: SIZE 
 	}).svg();
 
-	$qr.innerHTML = qr;
-	$save.href = "data:image/svg+xml;base64," + btoa(qr);
+	const qr_svg = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 800 800">${qr}</svg>`
+	const qr_png = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="800" height="800">${qr}</svg>`
+
+	$qr.innerHTML = qr_svg;
+	$exports[0].href = "data:image/svg+xml;base64," + btoa(qr_svg)
+
+	const $img = new Image;
+	$img.src = "data:image/svg+xml;base64," + btoa(qr_png)
+	$img.addEventListener("load", function(e){
+		const $canvas = document.createElement("canvas");
+		$canvas.width = this.width;
+		$canvas.height = this.height;
+
+		const $ctx = $canvas.getContext("2d");
+		$ctx.drawImage(this, 0, 0);
+		$exports[1].href = $canvas.toDataURL(); 
+	});
+
 }
 
 browser.tabs.query({currentWindow: true, active: true})
